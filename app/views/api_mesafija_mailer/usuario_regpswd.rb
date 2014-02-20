25fca7809b33fae531d7aabe281a2d6f579d3865
dbayo <david.bayo.nieto@gmail.com>
2014-02-20 01:39:23 +0100

    Modificar email que se envía en la función "usuario_regpswd"

--- app/views/api_mesafija_mailer/usuario_regpswd.rb
+++
@@ -1,5 +0,0 @@
-"Para regenerar su contrase&ntilde;a, pulse sobre el link que encontrar&aacute; a continuaci&oacute;n:"
-"<br /><br />"
-"<a href='http://www.mesafija.com/admin/regpswd.php?clave="+@user.clave+"'>Regenerar</a>"
-"<br /><br />"
-"El equipo de Mesafija.com"
